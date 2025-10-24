f619a9507c775a445d45eef85c968cc7b726b381
feiafeifei <lana.chen@efrei.net>
2025-10-24 10:48:10 +0200

    cmake

--- list.c
+++ list.c
@@ -1,1 +1,15 @@
 #include "list.h"
+
+
+void printList(t_list list){
+    if (is_empty(list)) {
+        printf("[]");
+    } else {
+        t_cell *cell = list;
+        while (cell != NULL) {
+            print_cell(*cell);
+            cell = cell->next;
+        }
+    }
+
+}
