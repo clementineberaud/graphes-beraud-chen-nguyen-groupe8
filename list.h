14d05768c966b0309eda2eb3f0376a81f9c6b7aa
Clémentine BERAUD <clementine.beraud@icloud.com>
2025-10-24 11:19:58 +0200

    list functions

--- list.h
+++ list.h
@@ -1,4 +1,5 @@
-
+#ifndef __LIST_H__
+#define __LIST_H__
 
 typedef struct s_cell {
   int sommet;
@@ -14,3 +15,8 @@ typedef struct s_list_adj {
   t_list *T;
   int taille;
 } t_list_adj;
+
+t_cell * createCell (int s, float p);
+t_list createEmptyList ();
+void addCell (t_list * list, int s, float p);
+#endif
