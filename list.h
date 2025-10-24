82e7e4fb9134fa376e80948314fd6627689fb008
Clémentine BERAUD <clementine.beraud@icloud.com>
2025-10-24 10:02:48 +0200

    struct list

---
+++ list.h
@@ -0,0 +1,16 @@
+
+
+typedef struct s_cell {
+  int sommet;
+  float proba;
+  struct s_cell* next;
+} t_cell;
+
+typedef struct s_list {
+  t_cell* head;
+} t_list;
+
+typedef struct s_list_adj {
+  t_list *T;
+  int taille;
+} t_list_adj;
