8818293eac6744570e97e66955ed26f1f8a41b47
Lana-chen1 <lana.chen@efrei.net>
2025-11-22 23:49:17 +0100

    Update tarjan.c
    fonction parcours

--- tarjan.c
+++ tarjan.c
@@ -32,3 +32,48 @@ int pop(t_stacklist* stack) {
     free(cell);
     return val;
 }
+
+
+
+void parcours(int v, t_list_adj* G, t_tab_tarjan tab, t_stacklist* P, int* num, int** partition, int* nbCFC){
+    tab[v].numero = *num;
+    tab[v].num_accessible = *num;
+    (*num)++;
+
+    push(P, v);
+    tab[v].indicateur = true;
+    
+    t_cell* cur = G->T[v].head;
+
+    while (cur != NULL) {
+        int w = cur->sommet;
+
+        if (tab[w].numero == -1) {
+            parcours(w, G, tab, P, num, partition, nbCFC);
+            if (tab[w].num_accessible < tab[v].num_accessible)
+                tab[v].num_accessible = tab[w].num_accessible;
+        }
+        else if (tab[w].indicateur) {
+            if (tab[w].numero < tab[v].num_accessible)
+                tab[v].num_accessible = tab[w].numero;
+        }
+
+        cur = cur->next;
+    }
+    
+    if (tab[v].num_accessible == tab[v].numero) {
+
+        int* cfc = malloc(G->taille * sizeof(int));
+        int cpt = 0;
+        int w;
+
+        do {
+            w = pop(P);
+            tab[w].indicateur = false;
+            cfc[cpt++] = w;
+        } while (w != v);
+
+        partition[*nbCFC] = cfc;
+        (*nbCFC)++;
+    }
+}
