8acc82f582ce706dea1a6a41afe25abc7f0b1232
Lana-chen1 <lana.chen@efrei.net>
2025-11-23 11:47:05 +0100

    Update tarjan.c

--- tarjan.c
+++ tarjan.c
@@ -77,3 +77,43 @@ void parcours(int sommet_curr, t_list_adj* graph_adj, t_tab_tarjan tab, t_stackl
         (*nbCFC)++;
     }
 }
+
+
+
+t_partitions tarjan(t_list_adj* graph){
+    int taille = graph->taille;
+
+    t_partitions part;
+    part.nb_composant = 0;
+    part.list_sommet = malloc(taille * sizeof(int*));
+    part.nb_sommet = malloc(taille * sizeof(int));
+
+    t_tab_tarjan tab = malloc(taille * sizeof(t_tarjan_vertex));
+
+    for (int i = 0; i < taille; i++) {
+        tab[i].num_sommet = i;
+        tab[i].numero = -1;
+        tab[i].num_accessible = -1;
+        tab[i].indicateur = false;
+    }
+
+    t_stacklist pile;
+    pile.head = NULL;
+
+    int num = 0;
+
+    for (int sommet_curr = 0; sommet_curr < taille; sommet_curr++) {
+        if (tab[sommet_curr].numero == -1) {
+            parcours(sommet_curr, graph, tab, &pile, &num, part.list_sommet, &part.nb_composant);
+        }
+    }
+
+    for (int cfc = 0; cfc < part.nb_composant; cfc++) {
+        int cpt = 0;
+        
+        while (cpt < taille && part.list_sommet[cfc][cpt] != -1) cpt++;
+        part.nb_sommet[cfc] = cpt;
+    }
+
+    return part;
+}
